dc136ef3e00b12ddf940168283aecbf9f7f3dc96
Gialbo <Gialbo@users.noreply.github.com>
2023-09-18 22:03:17 +0000

    jekyll build from Action 6db77d7b0a9cdf6fdf8cdd07464eb052b0e781d8

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Quantum Computing for Computer Engineers - Part 1",
+        "excerpt":"Qubits and quantum gates Here’s the thing about Quantum Computing: it is one of the most interesting and promising fields of research, but at the same time one of the most obscure. The idea of building a computer using the sometimes counter-intuitive laws of quantum mechanics is quite old, and...","categories": [],
+        "tags": ["Quantum Computing","Linear Algebra"],
+        "url": "/2022/11/01/quantum-computing-for-computer-engineers-part-1.html",
+        "teaser": null
+      },{
+        "title": "Quantum Computing for Computer Engineers - Part 2",
+        "excerpt":"Where are the other 432? The story so far: quantum computers run on strange little things called qubits, we can write them as \\(2\\)D vectors, and the values inside these vectors represent the probabilities of observing them in state \\(\\ket{0}\\) or \\(\\ket{1}\\). \\[\\ket{\\psi} = \\alpha\\ket{0} + \\beta\\ket{1} = \\alpha\\begin{bmatrix} 1...","categories": [],
+        "tags": ["Quantum Computing","Linear Algebra"],
+        "url": "/2023/01/31/quantum-computing-for-computer-engineers-part-2.html",
+        "teaser": null
+      },{
+        "title": "Quantum Computing for Computer Engineers - Part 3",
+        "excerpt":"So far, yet so close Imagine you have two bowling balls, one is black and one is white. You put these balls in two boxes, mix them up, and now you can’t tell which box contains which ball. Let’s suppose you gift one box to your friend who is about...","categories": [],
+        "tags": ["Quantum Computing","Linear Algebra"],
+        "url": "/2023/05/14/quantum-computing-for-computer-engineers-part-3.html",
+        "teaser": null
+      },{
+        "title": "The Quantum Game",
+        "excerpt":"A handbook for understanding quantum math in articles We all agree that quantum mechanics is weird and fascinating: it can teleport information, connect particles across space and time, and even speed up computations that would take centuries. Our mission at The Quantumist is to make this field a bit more...","categories": [],
+        "tags": ["Quantum Computing","Linear Algebra"],
+        "url": "/2023/07/19/the-quantum-game.html",
+        "teaser": null
+      },{
+        "title": "The snake biting its own tail: a brief overview of LLM collapse",
+        "excerpt":"Scientists in the 1940s encountered a particular problem: newly-produced equipment for detecting radiation was recording a faint but constant signal, making it impossible to detect radiation below a certain threshold, limiting its sensitivity. Nothing had changed about their manufacturing process, so what was the culprit? The cause - it was...","categories": [],
+        "tags": ["Machine Learning","Large Language Models"],
+        "url": "/2023/09/18/model-collapse.html",
+        "teaser": null
+      }]
